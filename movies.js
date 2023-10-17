e28b4d57eaa8aef1901e25d80eb8dbba2de57616
Rohit More <rohitbmore2002@gmail.com>
2023-10-17 21:57:10 +0530

    Initial Commit

---
+++ movies.js
@@ -0,0 +1,44 @@
+const input = document.getElementById("inp");
+const button = document.getElementById("btn");
+const movieslist = document.getElementById("movieslist");
+
+button.addEventListener("click", function () {
+  const searchText = input.value;
+  //   console.log(searchText);/
+  searchMovies(searchText);
+});
+
+function searchMovies(st) {
+  const apikey = "8d94ca69";
+  const apiurl = `http://www.omdbapi.com/?s=${st}&apikey=${apikey}`;
+
+  fetch(apiurl)
+    .then(function (res) {
+      return res.json();
+    })
+    .then(function (data) {
+      // console.log(data)
+      displayMovies(data);
+    })
+
+    .catch(function (err) {
+      console.error(err);
+    });
+}
+
+function displayMovies(data) {
+  if (data.Response === "True") {
+    data.Search.forEach(function (movie) {
+      const movieCard = document.createElement("div");
+      movieCard.classList.add("movieCard");
+      movieCard.innerHTML = `                
+                <img src=${movie.Poster} alt=${movie.Title}/>
+                <h2>${movie.Title}</h2>
+                <p>${movie.Year}</p>`
+
+      movieslist.appendChild(movieCard);
+    });
+  } else {
+    movieslist.innerHTML = `<strong>Movie Not Found..Search Another..</strong>`;
+  }
+}
